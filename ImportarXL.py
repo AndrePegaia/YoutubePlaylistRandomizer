bf9c80b67f4424db7bc8c3bde8df872e77539020
André Pegaia <andre.pegaia@gmail.com>
2023-02-05 10:06:32 -0300

    Adding executable branch

--- ImportarXL.py
+++
@@ -1,8 +0,0 @@
-def importarPlaylistXL(path):
-    pathPlaylistInfo = path + "/playlistinfo.xlsx"
-    pathVideosInfo = path + "/playlistVideosInfo.xlsx"
-
-    import pandas as pd
-    playlistImportada = pd.read_excel(pathPlaylistInfo)
-    playlistVideosImportados = pd.read_excel(pathVideosInfo)
-    return playlistImportada, playlistVideosImportados
